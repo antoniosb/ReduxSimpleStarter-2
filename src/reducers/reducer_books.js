36c2656434dc2eed948ab47415b80da2f8355ef9
Antônio Augusto <antonioabritto@gmail.com>
2017-12-03 21:52:59 -0200

    add book detail container

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    { title: 'javascript: the good parts' },
-    { title: 'Harry Potter' },
-    { title: 'The dark Tower' },
-    { title: 'Eloquent Ruby' }
+    { title: 'javascript: the good parts', pages: 101 },
+    { title: 'Harry Potter', pages: 45 },
+    { title: 'The dark Tower', pages: 567 },
+    { title: 'Eloquent Ruby', pages: 3 }
   ]
 }
